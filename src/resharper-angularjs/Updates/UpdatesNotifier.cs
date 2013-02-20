48acbfbe6313c540188c285d12504c870e1697a6
Matt Ellis <matt.ellis@jetbrains.com>
2013-02-20 15:03:31 +0000

    Update notifier support

---
+++ src/resharper-angularjs/Updates/UpdatesNotifier.cs
@@ -0,0 +1,71 @@
+﻿/*
+ * Copyright 2013 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using JetBrains.Application;
+using JetBrains.DataFlow;
+using JetBrains.UI.Updates;
+using JetBrains.VSIntegration.Updates;
+using System.Linq;
+
+namespace JetBrains.ReSharper.Plugins.AngularJS.Updates
+{
+    [ShellComponent]
+    public class UpdatesNotifier
+    {
+        public UpdatesNotifier(Lifetime lifetime, UpdatesManager updatesManager)
+        {
+            var uri = new Uri("https://raw.github.com/JetBrains/resharper-angularjs/master/updates.xslt");
+            var category = updatesManager.Categories.AddOrActivate("AngularJS", uri);
+            category.CustomizeLocalEnvironmentInfo.Advise(lifetime, args =>
+                {
+                    // We can customise the local environment info that the xslt will be applied to
+                    // It should be an instance of UpdateLocalEnvironmentInfo, bail out early if it's
+                    // not. The only reason it wouldn't be is if someone has got hold of the "AngularJS"
+                    // category and subscribed to the CustomizeLocalEnvironmentInfo signal. Unlikely.
+                    if (!(args.Out is UpdateLocalEnvironmentInfoVs))
+                        return;
+
+                    // Set the data the xslt will be applied against. Pass in the current environment,
+                    // in case we ever need it, but really, we only care about the current version
+                    args.Out = new PluginLocalEnvironmentInfo
+                        {
+                            LocalEnvironment = args.Out as UpdateLocalEnvironmentInfoVs,
+                            PluginVersion = new UpdateLocalEnvironmentInfo.VersionSubInfo(GetThisVersion())
+                        };
+                });
+
+            RemoveStaleUpdateNotification(category);
+        }
+
+        private static Version GetThisVersion()
+        {
+            var assembly = typeof(UpdatesNotifier).Assembly;
+            return assembly.GetName().Version;
+        }
+
+        private static void RemoveStaleUpdateNotification(UpdatesCategory category)
+        {
+            // ReSharper downloads and evaluates the xslt on a regular basis (every 24 hours),
+            // but doesn't re-evaluate it after an install (it doesn't know when something is
+            // installed!) so if there's a reminder to download this or an older version, remove it
+            var thisVersion = GetThisVersion();
+            var updateInfo = category.UpdateInfos.FirstOrDefault(container => new Version(container.Data.ProductVersion) <= thisVersion);
+            if (updateInfo != null)
+                category.UpdateInfos.Remove(updateInfo);
+        }
+    }
+}
---
+++ updates.xslt
@@ -0,0 +1,43 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+                xmlns:msxsl="urn:schemas-microsoft-com:xslt"
+                exclude-result-prefixes="msxsl">
+
+  <xsl:output method="xml" indent="yes"/>
+
+  <!-- This is the latest version available -->
+  <xsl:variable name="LatestMajor" select="1" />
+  <xsl:variable name="LatestMinor" select="2" />
+  <xsl:variable name="LatestBuild" select="0" />
+
+  <!-- Match the PluginLocalInfo element created by serialising the data from the category -->
+  <xsl:template match="/PluginLocalInfo">
+    <UpdateInfos>
+      <xsl:variable name="InstalledMajor" select="PluginVersion/@Major" />
+      <xsl:variable name="InstalledMinor" select="PluginVersion/@Minor" />
+      <xsl:variable name="InstalledBuild" select="PluginVersion/@Build" />
+    
+      <!-- If we have a new version, add an <UpdateInfo /> element to tell ReSharper a new version is ready -->
+      <xsl:if test="($InstalledMajor &lt; $LatestMajor) or ($InstalledMajor = $LatestMajor and $InstalledMinor &lt; $LatestMinor) or ($InstalledMajor = $LatestMajor and $InstalledMinor = $LatestMinor and $InstalledBuild &lt; $LatestBuild)">
+        
+        <UpdateInfo>
+          <InformationUri>https://github.com/JetBrains/resharper-angularjs/wiki/Release-Notes</InformationUri>
+          <Title>
+            <xsl:value-of select="concat('AngularJS Support for ReSharper ', $LatestMajor, '.', $LatestMinor, '.', $LatestBuild, ' Released')" />
+          </Title>
+          <Description>A minor upgrade is available.</Description>
+          <DownloadUri>
+            <xsl:value-of select="concat('http://download.jetbrains.com/resharper/plugins/resharper-angularjs.', $LatestMajor, '.', $LatestMinor, '.zip')" />
+          </DownloadUri>
+          <CompanyName>JetBrains</CompanyName>
+          <ProductName>AngularJS Support</ProductName>
+          <ProductVersion><xsl:value-of select="concat($LatestMajor, '.', $LatestMinor, '.', $LatestBuild, '.0')"/></ProductVersion>
+          <PriceTag />
+          <IsFree>true</IsFree>
+        </UpdateInfo>
+      
+      </xsl:if>
+    </UpdateInfos>
+  </xsl:template>
+
+</xsl:stylesheet>
