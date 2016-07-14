2a569791559e643e17de601eb252ec66c1b3106d
Matt Ellis <m.t.ellis@gmail.com>
2016-07-14 12:27:34 +0100

    Ignore duplicate definitions in the same file

--- src/resharper-angularjs/Feature/Services/Caches/AngularJsCacheItemsBuilder.cs
+++ src/resharper-angularjs/Feature/Services/Caches/AngularJsCacheItemsBuilder.cs
@@ -1,25 +1,30 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace JetBrains.ReSharper.Plugins.AngularJS.Feature.Services.Caches
 {
     public class AngularJsCacheItemsBuilder
     {
-        private readonly IList<Directive> directives = new List<Directive>();
-        private readonly IList<Filter> filters = new List<Filter>();
+        private readonly IDictionary<string, Directive> directives = new Dictionary<string, Directive>();
+        private readonly IDictionary<string, Filter> filters = new Dictionary<string, Filter>();
 
         public AngularJsCacheItems Build()
         {
-            return new AngularJsCacheItems(directives, filters);
+            return new AngularJsCacheItems(directives.Values.ToList(), filters.Values.ToList());
         }
 
         public void Add(Directive directive)
         {
-            directives.Add(directive);
+            // TODO: Should there be rules for precedence, merging?
+            if (!directives.ContainsKey(directive.Name))
+                directives.Add(directive.Name, directive);
         }
 
         public void Add(Filter filter)
         {
-            filters.Add(filter);
+            // TODO: Should there be rules for precedence, merging?
+            if (!filters.ContainsKey(filter.Name))
+                filters.Add(filter.Name, filter);
         }
     }
 }
